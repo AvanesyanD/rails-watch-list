0dd07cb46b6aa194104e2756c4121308b2b8f356
Davit Avanesyan <davitavanesyan13@gmail.com>
2024-11-28 16:54:24 +0100

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,13 +1,6 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
+
   validates :title, uniqueness: true, presence: true
   validates :overview, uniqueness: true, presence: true
-
-  before_destroy :check_for_bookmarks_association
-
-  protected
-
-  def check_for_bookmarks_association
-    self.bookmarks == nil
-  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,34 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+Movie.destroy_all
+
+puts "loading "
+
 Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
 Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+movie = Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+
+
+puts "Generated  #{Movie.count} Movies !"
+
+
+List.destroy_all
+
+puts "loading list"
+
+List.create(name: "Comedie")
+list = List.create(name: "Drama")
+List.create(name: "Fantasy")
+List.create(name: "Realistic")
+
+puts "Generated  #{List.count} lists !"
+
+Bookmark.destroy_all
+
+puts "loading Bookmark"
+
+Bookmark.create(comment: "Incroyable ", movie_id: movie.id, list_id: list.id)
+
+puts "Generated  #{Bookmark.count} bookmarks !"
